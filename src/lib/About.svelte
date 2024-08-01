239fefa1caef375645cbf86f66422179a89fc3ea
zulko <valentin.zulkower@gmail.com>
2024-08-01 00:39:10 -0400

    better about

--- src/lib/About.svelte
+++ src/lib/About.svelte
@@ -1,19 +1,19 @@
 <div class="About">
   <p>
-    This page explore the lives of famous composers, with major events collected
-    and summarized from Wikipedia by ChatGPT (read more <a
-      href="https://github.com/Zulko/composer-timelines/blob/main/docs/write-up.md"
+    This page explore the life and work of famous composers, with major events
+    collected and summarized from Wikipedia by ChatGPT (learn more <a
+      href="https://github.com/Zulko/composer-timelines/blob/main/data_collection/collect_data.ipynb"
       target="_blank">here</a
-    >), and lists of works scraped from <a href="https://imslp.org/">IMSLP</a>.
-    The project's
+    >), and composition lists scraped from
+    <a href="https://imslp.org/">IMSLP</a>. The project's
     <a href="https://github.com/Zulko/composer-timelines" target="_blank">
       Github page</a
     > explains how to report issues and add/request other composers.
   </p>
   <p>
-    Because all these people had depressing lives, I asked ChatGPT to "add some
-    fun" to the write-ups. However ChatGPT is very difficult to rein in when it
-    comes to humor. Watch it as it often misses the mark, abuses tired dad-puns,
+    Because all these people had depressing lives, I also asked ChatGPT to add
+    some fun in the write-ups, but it proved difficult to control. If that's
+    your thing, watch it as it often misses the mark, abuses tired dad-puns,
     resorts to stereotypes, and mocks tragic events through the lives of your
     favorite artists!
   </p>
